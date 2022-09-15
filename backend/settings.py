91a0acffdf28c8f9091d7e5780f1c36142372b95
git-anikm <anik.muktadir@devxhub.com>
2022-09-15 23:41:06 +0600

    allowed hosts edited

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e@&w1y41+^6e525*n4(!66cw#in^cu$llwe@v@t6l=zkd&=v3h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
